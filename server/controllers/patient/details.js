246eca23b4e58739ffa44b7d3c42e4206c5ab1f5
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-04-14 19:52:31 +0530

    Added details fetching in patient route

--- server/controllers/patient/details.js
+++ server/controllers/patient/details.js
@@ -1,3 +1,49 @@
-const getDetailsHandler = ()=>{
-    
+const {Patient, Appointment, Symptom} = require('../../models');
+
+const getAllDetailsHandler = async(req,res)=>{
+    const id = req.params.id
+    try {
+        const patient = await Patient.findOne({
+            where : {uuid : id},
+            include : ['symptoms', 'appointments', 'user'],
+        });
+        if (!patient) return res.status(404).json({message : 'Patient Not Found!'});
+        res.json(patient);
+    }
+    catch(err) {
+        console.log(err);
+        return res.status(400).json({message : 'Something went wrong!', error : err});
+    }
 }
+
+const getAppointmentsHandler = async(req,res)=>{
+    const id = req.params.id;
+    try {
+        const appointments = await Appointment.findAll({
+            where : {patientId : id},
+            include : ['patient', 'doctor']
+        });
+        res.json(appointments);
+    }catch(err) {
+        console.log(err);
+        return res.status(400).json({message : 'Something went wrong!', error : err});
+
+    }
+}
+
+const getAllSymptoms = async(req,res)=>{
+    const id = req.params.id;
+    try {
+        const symptoms = await Symptom.findAll({
+            where : {patientId : id},
+            include : ['patient']
+        });
+        res.json(symptoms);
+    }catch(err) {
+        console.log(err);
+        return res.status(400).json({message : 'Something went wrong!', error : err});
+
+    }
+}
+
+module.exports = {getAllDetailsHandler, getAppointmentsHandler, getAllSymptoms};
