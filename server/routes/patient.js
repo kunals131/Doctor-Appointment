07991d83ea1bd0216e7bd50d1145684c79274bdc
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-04-14 21:35:32 +0530

    Added routes for removing an speciality

--- server/routes/patient.js
+++ server/routes/patient.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getAllDetailsHandler, getAppointmentsHandler, getAllSymptoms, getAppointedDoctors } = require('../controllers/patient/details');
-const { addSymptomHandler, updatePatientHandler } = require('../controllers/patient/updateDetails');
+const { addSymptomHandler, updatePatientHandler, removeSymptomHandler } = require('../controllers/patient/updateDetails');
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.get('/:id/', getAllDetailsHandler);
 router.get('/:id/appointments/', getAppointmentsHandler);
 router.get('/:id/symptoms/', getAllSymptoms);
 router.post('/:id/symptoms', addSymptomHandler);
+router.delete('/:id/symptoms/:symptomId', removeSymptomHandler);
 router.get('/:id/appointeddoctors', getAppointedDoctors);
 router.put('/:id/update', updatePatientHandler)
 
