c999ea12c68d3938c3a11c96afdf0896e6d18e55
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-04-15 15:14:02 +0530

    initialized flask server

---
+++ flask-server/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from model import diseasePredictionModel
+app = Flask(__name__)
+
+prediction = diseasePredictionModel()
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if (request.method=='POST'):
+        symptoms = request.form['symptoms']
+        result = prediction(symptoms)
+        return jsonify(result)
+
+if (__name__=='__main__'):
+    app.run(debug=True)
