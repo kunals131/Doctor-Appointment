d14fe0fd6765a494d9f262b8b8e2cf53b97a6e5f
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-05-19 11:02:27 +0530

    Added Loading progress bar

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,7 +2,36 @@ import '../styles/globals.css'
 import Layout from '../components/Layout'
 import store from '../redux'
 import { Provider } from 'react-redux'
+import {useRouter} from 'next/router';
+import NProgress from 'nprogress';
+import '../public/nprogress.css'
+import { useEffect } from 'react';
+
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      console.log(`Loading: ${url}`)
+      NProgress.start()
+    }
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
+
+
   return  <>
   <Provider store={store}>
   <Layout>
